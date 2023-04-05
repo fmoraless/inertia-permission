e6f3b1374360182ca8ce1f779b7c006c50d4ae33
fmoraless <franticzero@gmail.com>
2023-04-05 02:36:00 -0400

    feat(permission-role): share roles and permissions

---
+++ resources/js/Pages/Admin/AdminIndex.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { Head } from '@inertiajs/vue3';
+import AdminLayout from "@/Layouts/AdminLayout.vue";
+</script>
+
+<template>
+    <Head title="Dashboard" />
+
+    <AdminLayout>
+        <div class="py-4">
+        <h1>Welcome admin</h1>
+        </div>
+    </AdminLayout>
+</template>
